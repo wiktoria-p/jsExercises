8e273b18ab9ac894f8125ea1efcb1f12e51baba6
Wiktoria Pietrzyk <83282608+wiktoria-p@users.noreply.github.com>
2022-12-17 11:28:51 +0100

    Add files via upload
    fucking java script again

---
+++ script.js
@@ -0,0 +1,79 @@
+// const number = Math.random() * 1000000
+// console.log(Math.floor (number));
+// // 246316
+
+// const kupon = 246316
+// if(number==kupon){
+//     console.log(`wygrałeś pierdyliard $`);
+// }
+
+// const test = document.getElementById('item1')
+// console.log(test);
+
+// const test2 = document.getElementsByTagName('li')
+// console.log(test2);
+
+// const test3 = document.getElementsByClassName('klasa')
+// console.log(test3);
+
+// const test4 = document.querySelector('li')
+// console.log(test4);
+
+// const test5 = document.querySelector('ul, li')
+// console.log(test5);
+
+// const ulList = document.querySelector('ul')
+// console.log(ulList);
+
+// const item1 = ulList.querySelector('li')
+// console.log(item1);
+
+// const test6 = document.querySelectorAll('li')
+// console.log(test6);
+
+// const heading = document.querySelector('h1')
+// console.log(heading);
+
+// const tablica = document.querySelectorAll('p')
+// console.log(tablica);
+
+// const test = document.querySelector('div')
+// console.log(test);
+
+// const p = document.querySelector('p')
+// console.log(p);
+
+// const ulList= document.querySelector('ul')
+// const newLi= document.createElement('li')
+// ulList.appendChild(newLi).textContent ('👀')
+
+// const test = document.querySelector('div')
+// const newP = document.createElement('p')
+// test.appendChild(newP).textContent='hejo'
+
+// const element = document.querySelector('div')
+// const list = document.createElement('ul')
+// const listElements = document.createElement('li')
+
+// element.appendChild(list).textContent= 'hejka'
+// list.appendChild(listElements).textContent ='naklejka'
+
+// Usuwanie elementu
+// document.body.removeChild(liElement)
+// div1234.removeChild(jakisParagraf)
+// liElement.remove()
+
+// appendi appendChild-dodawanie elementu
+// Za pomocą appendChild dodać możesz tylko jeden element.Natomiast append może dodać kilka elementów.
+// const div1234= document.querySelector('div')
+// const jakisParagraf= document.createElement('p')
+// jakisParagraf.textContent= 'To jest akapit'
+// constheadingOne= document.createElement('h1')
+// headingOne.textContent= 'Woohoo'
+// div1234.append(jakisParagraf, headingOne, 'dodatkowyteksttypstring')
+
+// addEventListener
+// constbtn2= document.querySelector('.btn2')
+// btn1.addEventListener('click', function() {
+    // console.log('liczękliknięcia');
+// })
